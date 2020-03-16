859750fec339a58d42cf7dc3f006a1e1555c52f4
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-16 06:24:54 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,16 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
-i=0
-newarray = []
-while i< collection.length
-  newarray << yield(collection[i])
-  i+=1
-end
+  i=0
+  newarray = []
+  while i< collection.length
+    newarray << yield(collection[i])
+    i+=1
+  end
 
-if newarray.include?(false)
-  false
-else
-true
-end
+  if newarray.include?(false)
+    false
+  else
+    true
+  end
 end
